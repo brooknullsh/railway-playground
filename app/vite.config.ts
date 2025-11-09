e3fac2c0a5d53e3e636a46b7b6e1f0fe90724db2
Brook Nash <brooknullsh@gmail.com>
2025-11-09 20:18:11 +0000

    fix: allowed host

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,4 +2,9 @@ import { sveltekit } from "@sveltejs/kit/vite"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] })
+export default defineConfig({
+  plugins: [tailwindcss(), sveltekit()],
+  server: {
+    allowedHosts: ["compassionate-nourishment-production.up.railway.app"],
+  },
+})
