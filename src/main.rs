0995692e49362b53677d7ee7aa2425c13595011b
Brook Nash <brooknullsh@gmail.com>
2025-12-21 19:32:35 +0000

    chore: docs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::process::exit;
 use std::str::FromStr;
 use std::sync::Arc;
 
+use anyhow::Result;
 use axum::Router;
 use axum::middleware::from_fn_with_state;
 use axum::routing::get;
@@ -18,22 +19,22 @@ use tracing::error;
 use tracing::info;
 use tracing_subscriber::fmt;
 
-use crate::handler::auth;
+use crate::handler::auth::auth;
+use crate::handler::auth::login;
 use crate::handler::index;
-use crate::handler::login;
 
 mod handler;
 
 #[derive(Clone)]
-struct AppState
-{
+struct AppState {
   pool: Arc<PgPool>,
-  acc_secret: String,
-  ref_secret: String,
+  secret: String,
 }
 
-fn setup_logging()
-{
+/// Create a logger to STDOUT based on an environment variable or default.
+///
+/// NOTE: Used within the handler tests.
+fn setup_logging() {
   let env_lvl = "DEBUG".to_string();
   let env_lvl = var("LOG_LEVEL").unwrap_or(env_lvl);
   let log_lvl = Level::from_str(&env_lvl).unwrap_or(Level::DEBUG);
@@ -44,24 +45,18 @@ fn setup_logging()
     .compact()
     .init();
 
-  info!("logging lvl: {}", log_lvl);
+  info!("logging lvl: {log_lvl}");
 }
 
-async fn create_app() -> anyhow::Result<Router>
-{
-  let db = var("DATABASE_URL")?;
-  let acc_secret = var("ACCESS_SECRET")?;
-  let ref_secret = var("REFRESH_SECRET")?;
-
+/// Initialise the database pool, handlers and [`AppState`].
+///
+/// NOTE: Used within the handler tests.
+async fn create_app() -> Result<Router> {
+  let (db, secret) = (var("DATABASE_URL")?, var("TOKEN_SECRET")?);
   let pool = PgPoolOptions::new().connect(&db).await?;
   let pool = Arc::new(pool);
 
-  let state = AppState {
-    pool,
-    acc_secret,
-    ref_secret,
-  };
-
+  let state = AppState { pool, secret };
   let auth_ware = from_fn_with_state(state.clone(), auth);
   let trace_lyr = TraceLayer::new_for_http();
   let cookie_lyr = CookieManagerLayer::new();
@@ -78,32 +73,30 @@ async fn create_app() -> anyhow::Result<Router>
 }
 
 #[tokio::main]
-async fn main()
-{
+async fn main() {
   setup_logging();
-
-  let Ok(app) = create_app()
-    .await
-    .inspect_err(|err| error!("[SETUP] {err:#}"))
-  else
-  {
+  let Ok(app) = create_app().await.inspect_err(|err| error!("{err}")) else {
     exit(1)
   };
 
-  let port = "8080".to_string();
-  let port = var("PORT").unwrap_or(port);
-  let host = format!("0.0.0.0:{}", port);
-
-  let Ok(listener) = TcpListener::bind(&host)
-    .await
-    .inspect_err(|err| error!("[SETUP] {err:#}"))
-  else
-  {
-    exit(1)
+  let host = {
+    let port = "8080".to_string();
+    let port = var("PORT").unwrap_or(port);
+    format!("0.0.0.0:{port}")
   };
 
-  // On error, the TCP listener sleeps (for a second) without returning an error
-  // so unwrap the result regardless.
-  info!("starting at: {host}");
-  serve(listener, app).await.unwrap();
+  match TcpListener::bind(&host).await {
+    Ok(listener) => {
+      // NOTE: This can be unwrapped as the error handling is just to sleep.
+      //
+      // "Errors on the TCP socket will be handled by sleeping for a short while
+      // (currently, one second)" - axum::serve
+      info!("starting at: {host}");
+      serve(listener, app).await.unwrap();
+    }
+    Err(err) => {
+      error!("{err}");
+      exit(1)
+    }
+  }
 }
